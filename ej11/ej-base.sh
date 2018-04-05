5c978da570fc0facd6bbcd19372c70cd9d10ba92
danibarria <barriareyesjosedaniel@gmail.com>
2018-04-05 09:11:25 -1100

    archivos creados

--- ej11/ej-base.sh
+++ ej11/ej-base.sh
@@ -2,17 +2,18 @@
 #
 # shellscript1
 if [ $# -ne 1 ]; then
-echo "uso: shellscript1 <patron> | -h"
-exit 1
+    echo "uso: shellscript1 <patron> | -h"
+    exit 1
 fi
 if [ $1 = "-h" ]; then
-echo "Texto explicativo."
-exit 0
+    echo "Texto explicativo."
+    exit 0
 fi
 PATRON=$1
-for i in `find ./ -name “*.[hc]”`
+#echo $1
+for i in `find . -name “*.[hc]”`
 do
-echo "--------------"
-echo $i
-grep $PATRON $i
+    echo "--------------"
+    echo $i
+    grep $PATRON $i
 done
--- ej11/explicacion.md
+++ ej11/explicacion.md
@@ -1,1 +1,16 @@
-cualquier archivo de punto h o punto c
+si la cantidad de parametros no es 1
+    muestra el uso y sale
+
+si el primer parametro es -h
+    muestra el texto explicativo y sale
+
+toma del primer argumento un patron
+por cada archivo encontrado con extension .h o .c
+    muestra ------
+    muestra lo que hay en $i
+    y busca $patron en $i
+
+
+
+
+----cualquier archivo de punto h o punto c
