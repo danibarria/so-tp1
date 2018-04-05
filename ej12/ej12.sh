0f5470d0279af39e25f111e47c8814b041f1ca1c
danibarria <barriareyesjosedaniel@gmail.com>
2018-04-05 08:27:22 -1100

    agregada longitud OK

--- ej12/ej12.sh
+++ ej12/ej12.sh
@@ -5,8 +5,9 @@ read ciudad
 base="https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA3memeBiMaEPPLY7zAp4TzA2rqcLo0C1w&new_forward_geocoder=true&address="
 
 curl $base$ciudad > ciudad.json
-
+echo "latitud"
 cat ciudad.json | grep "lat"
-
+echo "longitud"
+cat ciudad.json | grep "lng"
 #wget $base$ciudad 
 #wget https://maps.googleapis.com/maps/api/geocode/json?address=trelew > ciudad.json
