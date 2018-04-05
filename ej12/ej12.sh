88e4676f5966c9b4e6378c67bb73ea8a9304e332
danibarria <barriareyesjosedaniel@gmail.com>
2018-04-05 08:25:03 -1100

    ejercicio 12 terminado

--- ej12/ej12.sh
+++ ej12/ej12.sh
@@ -2,10 +2,11 @@
 
 echo "ingrese ciudad"
 read ciudad
-base="https://maps.googleapis.com/maps/api/geocode/json?address="
+base="https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA3memeBiMaEPPLY7zAp4TzA2rqcLo0C1w&new_forward_geocoder=true&address="
 
 curl $base$ciudad > ciudad.json
 
 cat ciudad.json | grep "lat"
+
 #wget $base$ciudad 
 #wget https://maps.googleapis.com/maps/api/geocode/json?address=trelew > ciudad.json
