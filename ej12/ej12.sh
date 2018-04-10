858e066916a5dd229df4ebf09c290b77b8e06db3
danibarria <barriareyesjosedaniel@gmail.com>
2018-04-10 10:46:13 -1100

    arreglado 12

--- ej12/ej12.sh
+++ ej12/ej12.sh
@@ -5,9 +5,16 @@ read ciudad
 base="https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA3memeBiMaEPPLY7zAp4TzA2rqcLo0C1w&new_forward_geocoder=true&address="
 
 curl $base$ciudad > ciudad.json
-echo "latitud"
-cat ciudad.json | grep "lat"
-echo "longitud"
-cat ciudad.json | grep "lng"
+if [ $? ]
+then
+    echo "latitud"
+    cat ciudad.json | grep "lat" -m 1
+    echo "longitud"
+    cat ciudad.json | grep "lng" -m 1
+else
+    echo "instala curl"
+fi
+
+
 #wget $base$ciudad 
 #wget https://maps.googleapis.com/maps/api/geocode/json?address=trelew > ciudad.json
